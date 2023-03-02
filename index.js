535101a8cb4170c322cbb92961169820d8d586d0
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-03-02 12:31:16 -0800

    template added and copy, read, write functionality

--- index.js
+++ index.js
@@ -1,9 +1,107 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.createPromptModule([
-  {
- 
-  },
-])
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Title of the project:",
+    },
+    {
+      type: "input",
+      name: "siteLink",
+      message: "Enter url to published site if applicable:",
+    },
+    {
+      type: "input",
+      name: "demoLink",
+      message: "Enter url to live demo if applicable:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message:
+        "Write a description of the project. What was your motivation? Why did you build this project? What problem does it solve? What did you learn?",
+    },
+    {
+      type: "input",
+      name: "features",
+      message:
+        "If your project has a lot of features, list them here. Start item with asterisk (*) and end with <br> to generate list items.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use:",
+    },
+    {
+      type: "input",
+      name: "screenshot",
+      message:
+        "To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, write the filepath here.",
+    },
+    {
+      type: "input",
+      name: "diagram",
+      message:
+        "To add a diagram, create an `assets/images` folder in your repository and upload your diagram to it. Then, write the filepath here.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message:
+        "What are the steps required to install your project? Provide step-by-step description of how to get the project running. Start item with asterisk (*) and end with <br> to generate list items.",
+    },
+    {
+      type: "input",
+      name: "dependencies",
+      message:
+        "What are the dependencies required to run the program? Start item with asterisk (*) and end with <br> to generate list items.",
+    },
+    {
+      type: "input",
+      name: "credits",
+      message:
+        "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well. Start item with asterisk (*) and end with <br> to generate list items.",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "License:",
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.title
+      .toLowerCase()
+      .split(" ")
+      .join("+")}_README.md`;
 
+    fs.copyFile("template.md", `./output/${filename}`, (err) => {
+      if (err) {
+        console.log(`Error found: ${err}`);
+      } else {
+        console.log(`Successful Copy.`);
+      }
+
+      fs.readFile(`./output/${filename}`, "utf-8", (err, contents) => {
+        if (err) {
+          console.log(`Error reading file: ${err}`);
+          return;
+        } else {
+          console.log(`Successful Read.`);
+        }
+
+        const replaced = contents.replaceAll("%description", data.description);
+
+        fs.writeFile(`./output/${filename}`, replaced, "utf-8", (err) => {
+          if (err !== null) {
+            console.log(`Error writing file: ${err}`);
+            return;
+          } else {
+            console.log(`Successful Write.`);
+          }
+        });
+      });
+    });
+  });
---
+++ template.md
@@ -0,0 +1,67 @@
+# ${data.title}
+
+Link to site: [Site](${data.siteLink})
+
+Link to live demo: [Demo](${data.demoLinK)
+
+## Table of Contents
+
+- [Description](#description)
+- [Features](#features)
+- [Usage](#usage)
+- [Installation](#installation)
+- [Dependencies](#dependencies)
+- [Credits](#credits)
+- [License](#license)
+
+## Description
+
+%description
+
+## Features
+
+${data.features}
+
+## Usage
+
+${data.usage}
+
+[screenshot](${data.screenshot})
+
+[diagram](${data.diagram})
+
+## Installation
+
+${data.installation}
+
+## Dependencies
+
+${data.dependencies}
+
+## Credits
+
+${data.credits}
+
+---
+
+## License
+
+${data.license}
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
